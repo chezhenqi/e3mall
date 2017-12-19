2fcd043c4a86de7244ea373c51c881e2ea6e645d
chezhenqi <chezhenqi_test@126.com>
2017-12-19 23:45:59 +0800

    第二天

--- m_e3_manager/m_e3_manager_interface/src/main/java/cn/che/e3mall/service/TbItemService.java
+++ m_e3_manager/m_e3_manager_interface/src/main/java/cn/che/e3mall/service/TbItemService.java
@@ -1,7 +1,10 @@
 package cn.che.e3mall.service;
 
+import cn.che.e3mall.common.pojo.DataGridResult;
 import cn.che.e3mall.pojo.TbItem;
 
 public interface TbItemService {
 	TbItem selectById(Long id) throws Exception;
+
+	DataGridResult findByPage(int page, int rows);
 }
--- m_e3_manager/m_e3_manager_service/src/main/java/cn/che/e3mall/service/impl/TbItemServiceImpl.java
+++ m_e3_manager/m_e3_manager_service/src/main/java/cn/che/e3mall/service/impl/TbItemServiceImpl.java
@@ -1,10 +1,17 @@
 package cn.che.e3mall.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+
+import cn.che.e3mall.common.pojo.DataGridResult;
 import cn.che.e3mall.mapper.TbItemMapper;
 import cn.che.e3mall.pojo.TbItem;
+import cn.che.e3mall.pojo.TbItemExample;
 import cn.che.e3mall.service.TbItemService;
 
 @Service
@@ -15,6 +22,19 @@ public class TbItemServiceImpl implements TbItemService {
 	@Override
 	public TbItem selectById(Long id) throws Exception {
 		return tbItemMapper.selectByPrimaryKey(id);
+	}
+
+	@Override
+	public DataGridResult findByPage(int page, int rows) {
+		// 查询前先进性分页处理
+		PageHelper.startPage(page, rows);
+		TbItemExample example = new TbItemExample();
+		List<TbItem> list = tbItemMapper.selectByExample(example);
+		PageInfo<TbItem> pageInfo = new PageInfo<>(list);
+		DataGridResult dataGridResult = new DataGridResult();
+		dataGridResult.setTotal(pageInfo.getTotal());
+		dataGridResult.setRows(pageInfo.getList());
+		return dataGridResult;
 	}
 
 }
